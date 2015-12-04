4e784b75acdfeae239c8a4f46a78dc7bb393aa3f
hieunh161 <hieunh161@gmail.com>
2015-12-05 00:51:21 +0900

    add test when logged in but is not friend

--- src/main/java/jp/co/fjp/user/User.java
+++ src/main/java/jp/co/fjp/user/User.java
@@ -1,12 +1,25 @@
 package jp.co.fjp.user;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import jp.co.fjp.tripservice.Trip;
 
 public class User {
 
 	public List<User> getFriends() {
 		// TODO Auto-generated method stub
-		return null;
+		return new ArrayList<User>();
+	}
+
+	public void addFriend(User anotherUser) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addTrip(Trip toBrazil) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- src/test/java/jp/co/fjp/tripservice/TripServiceTest.java
+++ src/test/java/jp/co/fjp/tripservice/TripServiceTest.java
@@ -1,25 +1,49 @@
 package jp.co.fjp.tripservice;
 
+import java.util.List;
+
 import jp.co.fjp.exception.DependentClassCallDuringUnitTestException;
 import jp.co.fjp.exception.UserNotLoggedInException;
 import jp.co.fjp.user.User;
 
+import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TripServiceTest {
+	private static final User GUESS = null;
+	private static final User UNUSED_USER = null;
+	private static final User REGISTERED_USER = new User();
+	private static final User ANOTHER_USER = new User();
+	private static final Trip TO_BRAZIL = new Trip();
+	private User loggedInUser;
+
 	@Test(expected = UserNotLoggedInException.class)
 	public void should_throw_exception_when_user_is_not_logged_in()
 			throws UserNotLoggedInException,
 			DependentClassCallDuringUnitTestException {
 		TripService tripService = new TestableTripService();
-		tripService.getTripsByUser(null);
+		loggedInUser = GUESS;
+		tripService.getTripsByUser(UNUSED_USER);
 	}
-	
-	private class TestableTripService extends TripService{
+
+	@Test
+	public void should_not_return_any_trip_when_users_are_not_friends()
+			throws Exception {
+		TripService tripService = new TestableTripService();
+		loggedInUser = REGISTERED_USER;
+		User friend = new User();
+		friend.addFriend(ANOTHER_USER);
+		friend.addTrip(TO_BRAZIL);
+		List<Trip> friendTrips = tripService.getTripsByUser(friend);
+		Assert.assertThat(friendTrips.size(), Matchers.is(0));
+	}
+
+	private class TestableTripService extends TripService {
 		@Override
-		protected User getLoggedInUser(){
-			return null;
-			
+		protected User getLoggedInUser() {
+			return loggedInUser;
+
 		}
 	}
 
